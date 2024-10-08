e7d76e364e56da3eefc5a2a68d281f5a9de6aecb
Rama116 <ramagugan77@gmail.com>
2024-10-08 09:09:38 +0530

    Signup and login frontend Over

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,36 +16,36 @@ const Navbar = () => {
         setMenu(false);
     }
 
-    return (
+    return (    
         <>
             <div className="fixed w-full">
                 <div>
                     <div className="flex flex-row justify-between p-5 md:px-32 px-5 bg-white shadow-[0_3px_10px_rgba(0,0,0,0.2)]">
                         <div className="flex flex-row items-center cursor-pointer">
-                            <span>
+                            {/* <span>
                                 <BiRestaurant size={32} />
-                            </span>
-                            <h1 className="text-xl font-semibold">FoodieWeb</h1>
+                            </span> */}
+                            <h1 className="text-xl font-semibold">Title</h1>
                         </div>
                         <nav className="hidden md:flex flex-row items-center text-lg font-medium gap-8">
-                            <Link to="home" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Home</Link>
+                            <Link to="home" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link1</Link>
                             <div className="relative group">
                                 <div className="flex items-center gap-1">
-                                    <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Dishes</Link>
+                                    <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link2</Link>
                                     <BiChevronDown className="cursor-pointer" size={25} />
                                 </div>
 
                                 <ul className="absolute hidden space-y-2 group-hover:block bg-white border-2 border-gray-300 rounded-lg p-5">
 
-                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Spicy</Link> </li>
-                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Tasty</Link> </li>
-                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Delicious</Link> </li>
-                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Crispy</Link> </li>
+                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >l2sub1</Link> </li>
+                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >l2sub2</Link> </li>
+                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >l2sub3</Link> </li>
+                                    <li> <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >l2sub4</Link> </li>
 
                                 </ul>
                             </div>
-                            <Link to="about" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >About</Link>
-                            <Link to="menu" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Menu</Link>
+                            <Link to="about" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link3</Link>
+                            <Link to="menu" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link4</Link>
 
                             <Button title="Login" />
                         </nav>
@@ -56,10 +56,10 @@ const Navbar = () => {
                         </div>
                     </div>
                     <div className={`${menu ? "translate-x-0" : "-translate-x-full"} lg-hidden flex flex-col absolute bg-black text-white left-0 top-20 font-semibold text-2xl text-center pt-8 pb-4 gap-8 w-full h-fit transition-transform duration-300 `}>
-                        <Link to="home" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Home</Link>
-                        <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Dishes</Link>
-                        <Link to="about" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >About</Link>
-                        <Link to="menu" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Menu</Link>
+                        <Link to="home" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link1</Link>
+                        <Link to="dishes" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link2</Link>
+                        <Link to="about" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link3</Link>
+                        <Link to="menu" spy={true} smooth={true} duration={500} className="hover:text-orange-400 transition-all cursor-pointer" >Link4</Link>
                     
                         <Button title="Login" />
                     </div>
